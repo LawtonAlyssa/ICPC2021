8d84fcf1329b357419d1b932a775ee181622f9eb
LawtonAlyssa <46826882+LawtonAlyssa@users.noreply.github.com>
2021-03-06 23:06:16 +0000

    Add files via upload

--- aProblemZ.java
+++ aProblemZ.java
@@ -8,12 +8,12 @@ public class aProblemZ {
         int c = 1;
         for(int i=0; i<=n; i++){
             int d1;
-            if(i>=9){
+            if(c>9){
                 d1 = (int)(d.charAt(i)-'0')*10+(int)(d.charAt(i+1)-'0');
                 i++;
             }
             else
-                d1 = (int)(d.charAt(i)-'0');
+                d1 = (int)(d.charAt(i)-'0');  
             if(d1!=c){
                 System.out.println(c);
                 System.exit(0);
