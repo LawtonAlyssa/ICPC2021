1279e4b0cf2bf3d459267763f824c38e33b245ca
lamquangtruongnguyen <77467939+lamquangtruongnguyen@users.noreply.github.com>
2021-03-06 10:52:57 -1000

    Add files via upload

--- aProblemS.java
+++ aProblemS.java
@@ -1,50 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hpu.cs.tnguyen10.icpccontest;
+
 import java.util.*;
-public class aProblemS{
+
+public class aProblemS {
+
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         int k = kb.nextInt();
+        kb.nextLine();
         String t;
         char[][] a = new char[n][k];
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             t = kb.next();
-            for(int j = 0; j<k; j++){
+            for (int j = 0; j < k; j++) {
                 a[i][j] = t.charAt(j);
             }
         }
-        boolean m = true;
-        int c=0;
-        System.out.println("hi");
-        for(int j = 1; j<k; j++){
-            System.out.println("hi");
-            for(int i = 0; i<n; i++){
-                char a0 = a[i][0];
-                System.out.println(a[i][j]+" "+a0);
-                if(a[i][j]!=a0){
+
+        boolean m;
+        int c = 0;
+
+        for (int j = 0; j < k; j++) {
+            m = true;
+            for (int i = 0; i < n; i++) {
+                if (a[i][j] != a[0][j]) {
                     m = false;
                     break;
                 }
             }
-            System.out.println(m);
-            if(m)
-                c++;
+            
+            if (m == true) c++;
+               
         }
-
-        /*for(int i = 0; i<n; i++){
-            char a0 = a[i][0];
-            for(int j = 1; j<k; j++){
-                if(a[i][j]!=a0){
-                    m = false;
-                    System.exit(0);
-                }
-                else
-                    a0 = a[i][j];
-
-            }
-
-        }*/
-
-        //System.out.println(a[0][0]==a[1][0]);
-        System.out.println("HI");
+        System.out.println(c);
     }
 }
