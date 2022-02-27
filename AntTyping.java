50689bb5678a22d16bdf26f58b93c6151d6de74c
Edward Flores <EdwardVFlores@gmail.com>
2022-02-27 07:57:22 -1000

    subtracted for grid

--- AntTyping.java
+++ AntTyping.java
@@ -18,7 +18,7 @@ public class AntTyping {
         boolean[][] visited = new boolean[3][3];
         for (int i = 1; i <= 3; i++) {
             for (int j = 1; j <= 3; j++) {
-                grid[i][j] = i*j;
+                grid[i-1][j-1] = i*j;
             }
         }
         int answer = 0;
